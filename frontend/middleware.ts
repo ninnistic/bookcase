5a2b3bd452579fd2968dc05dae12b684085fb731
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-05-16 01:04:58 +0900

    protect routes w/ middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,15 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+import { getUserMeLoader } from "@/app/services/get-user-me-loader";
+
+export async function middleware(request: NextRequest) {
+  const user = await getUserMeLoader();
+  const currentPath = request.nextUrl.pathname;
+
+  if (currentPath.startsWith("/dashboard") && user.ok === false) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
+}
