6bfde63d2fa74a164f3fe5c8ff0e99321d9b7b00
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-05-16 00:38:50 +0900

    set http only cookie

--- frontend/app/actions/auth-actions.ts
+++ frontend/app/actions/auth-actions.ts
@@ -1,8 +1,18 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
 import { registerUserService } from "../services/auth-service";
+
+const config = {
+  maxAge: 60 * 60 * 24 * 7, // 1 week
+  path: "/",
+  domain: process.env.HOST ?? "localhost",
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+};
 
 const schemaRegister = z.object({
   username: z.string().min(3).max(10, {
@@ -54,8 +64,6 @@ export async function registerUserAction(prevState: any, formData: FormData) {
     };
   }
 
-  return {
-    ...prevState,
-    data: "ok",
-  };
+  cookies().set("jwt", responseData.jwt, config);
+  redirect("/dashboard");
 }
